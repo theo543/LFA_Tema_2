9212ff842b5316c878f9a93e3241687c8d525f59
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-05-05 16:40:54 +0300

    Add a redundant DFA test (it works?!)
    So.. maybe determinized NFAs just aren't redundant?
    TODO: add an external FA library and see if it gets the same minimized size
    
    Add some links

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <functional>
+#include <vector>
 #include "DFA.h"
 #include "NFA.h"
 bool bruteforce_strings(const std::string &prev, int size, int maxsize, const std::function<bool(const std::string &)> &callback) {
@@ -14,6 +15,29 @@ bool bruteforce_strings(const std::string &prev, int size, int maxsize, const st
     }
     return false;
 }
+DFA manualtestDFA() {
+    // from here: https://www.geeksforgeeks.org/minimization-of-dfa/
+    DFA manualtest;
+    manualtest.resize(8);
+    manualtest.setStartState(6);
+    manualtest.setFinalState(1, true);
+    manualtest.setFinalState(2, true);
+    manualtest.setFinalState(4, true);
+    constexpr Transition tr[] = {
+            {0, 'a', 3}, {0, 'b', 1},
+            {1, 'a', 2}, {1, 'b', 5},
+            {2, 'a', 2}, {2, 'b', 5},
+            {3, 'a', 0}, {3, 'b', 4},
+            {4, 'a', 2}, {4, 'b', 5},
+            {5, 'a', 5}, {5, 'b', 5},
+            {6, 'x', 7}, {6, 'y', 0},
+            {7, 'x', 7}, {7, 'y', 0},
+    };
+    for(auto &t : tr) {
+        manualtest.addTransition(t);
+    }
+    return manualtest;
+}
 int main() {
     std::cout<<"Enable debug output? (1/0): ";
     {
@@ -22,23 +46,44 @@ int main() {
         setDebugOutputEnabled(debug);
         std::ios::sync_with_stdio(false);
     }
-    std::cout << "Enter path to NFA file: ";
-    std::string path;
-    std::cin >> path;
-    NFA nfa = NFA::deserialize(path);
-    std::cout << "NFA created with " << nfa.getSize() << " states" << std::endl;
-    DFA unminimized = nfa.determinize();
-    std::cout << "DFA created with " << unminimized.getSize() << " states" << std::endl;
+    struct check {
+        std::string name;
+        FA *fa;
+    };
+    std::vector<check> checks;
+    ///TODO add an external library for extra checking (this https://github.com/katef/libfsm looks useful if it has minimization)
+    NFA nfa;
+    DFA unminimized;
+    std::cout<<"Use manual test DFA? (1/0): ";
+    {
+        int manual;
+        std::cin >> manual;
+        if (!manual) {
+            std::cout << "Enter path to NFA file: ";
+            std::string path;
+            std::cin >> path;
+            nfa = NFA::deserialize(path);
+            std::cout << "NFA created with " << nfa.getSize() << " states" << std::endl;
+            unminimized = nfa.determinize();
+            std::cout << "DFA created with " << unminimized.getSize() << " states" << std::endl;
+            checks.emplace_back("Deserialized NFA", &nfa);
+        } else {
+            unminimized = manualtestDFA();
+            std::cout << "Manual test DFA created with " << unminimized.getSize() << " states" << std::endl;
+        }
+    }
     unminimized.print(logger());
     DFA shaken = unminimized.treeshake(); // just to make sure this works even on its own
-    DFA dfa = unminimized.minimize();
-    std::cout << "Minimized DFA created with " << dfa.getSize() << " states" << std::endl;
-    dfa.print(logger());
+    DFA minimized = unminimized.minimize();
+    std::cout << "Minimized DFA created with " << minimized.getSize() << " states" << std::endl;
+    minimized.print(logger());
     std::cout<< "Type \"exit\" to exit\n";
-    std::cout<< "Valid string example from final DFA:" << dfa.get_valid_string() << std::endl;
+    std::cout << "Valid string example from final DFA:" << minimized.get_valid_string() << std::endl;
     std::cout<< "Valid string from unminimized DFA:" << unminimized.get_valid_string() << std::endl;
     std::string input;
-    FA *checks[] = {&nfa, &unminimized, &shaken, &dfa};
+    checks.emplace_back("Unminimized DFA", &unminimized);
+    checks.emplace_back("Minimized DFA", &minimized);
+    checks.emplace_back("Shaken DFA", &shaken);
     while(true) {
         std::cout << "Input string (_ = lambda) (BF = search for bugs) : ";
         std::cin >> input;
@@ -47,8 +92,9 @@ int main() {
         if (input == "BF") {
             long long bfcounter = 0, yescounter = 0, maxfinds;
             std::function<bool(const std::string&)> checkequal = [&checks, &bfcounter, &yescounter, &maxfinds](const std::string &s) -> bool {
-                bool result = checks[0]->tryAccept(s);
-                for (FA *check: checks) {
+                bool result = checks[0].fa->tryAccept(s);
+                for (auto &check_pair : checks) {
+                    FA *check = check_pair.fa;
                     if (check->tryAccept(s) != result) {
                         std::cout << "Bug found!\n";
                         std::cout << "Cause string: " << s << "\n";
@@ -59,7 +105,7 @@ int main() {
                 }
                 bfcounter++;
                 if(result) yescounter++;
-                if(bfcounter % 100000 == 0) std::cout << "Checked " << bfcounter << " strings, " << yescounter << " of which were accepted" << std::endl;
+                if(bfcounter % 10000000 == 0) std::cout << "Checked " << bfcounter << " strings, " << yescounter << " of which were accepted" << std::endl;
                 return false;
             };
             std::cout << "Enter max string length: ";
@@ -74,19 +120,15 @@ int main() {
             }
             continue;
         }
-        bool result = checks[0]->tryAccept(input);
-        for (FA *check: checks) {
-            if (check->tryAccept(input) != result)
-                goto debug_output;
+        bool result = checks[0].fa->tryAccept(input);
+        bool bugged = false;
+        for (auto &check_pair : checks) {
+            FA *check = check_pair.fa;
+            bool thisresult = check->tryAccept(input);
+            std::cout << check_pair.name << ": " << (thisresult ? "Yes" : "No") << std::endl;
+            bugged |= thisresult != result;
         }
-        std::cout << (result ? "Yes" : "No") << std::endl;
-        continue;
-        debug_output:
-        std::cout<<"Bug found!\n";
-        std::cout<< "NFA result: " << (checks[0]->tryAccept(input) ? "Yes" : "No") << std::endl;
-        std::cout<< "Unminimized DFA result: " << (checks[1]->tryAccept(input) ? "Yes" : "No") << std::endl;
-        std::cout<< "Treeshaken DFA result: " << (checks[2]->tryAccept(input) ? "Yes" : "No") << std::endl;
-        std::cout<< "Minimized DFA result: " << (checks[3]->tryAccept(input) ? "Yes" : "No") << std::endl;
+        std::cout << (bugged ? "Bug found!" : "All match!") << std::endl;
     }
     return 0;
 }
